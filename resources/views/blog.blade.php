c59ea62532d2f809ae67816de64f79e60afc1f6c
Felicia Evelina Soetjipto <147375205+feliciaeve@users.noreply.github.com>
2023-11-13 15:34:25 +0700

    pertemuan 13 Nov

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tutorial Laravel - www.mlasngoding.com</title>
+</head>
+<body>
+    <h3>www.malasngoding.com</h3>
+    <p>Seri Tutorial Laravel Lengkap Dari dasar</p>
+    <p>Ini adalah view blog, ada di route blog.</p>
+
+    <a href="/pegawai/{{ date ('D M Y') }}">Klik Disini</a>
+</body>
+</html>
